4593d9c6778971c8b0cdf33ff478d90efb092890
gingerale <gingeralesy@gmail.com>
2022-01-15 10:26:15 +0200

    Typo

--- genes.lisp
+++ genes.lisp
@@ -1,13 +1,13 @@
 (in-package #:org.shirakumo.fraf.vpetjam)
 
-(defun genomes ()
-  (list :hue :speed :face :body :hat))
+(defparameter *genomes* '(:hue :speed :face :body :hat))
 
 (defun gene-initial (genome)
   (declare (type keyword genome))
   (case genome
-    (:hat 0)
-    (T (alexandria:random-elt (gene-potential genome)))))
+    (:speed 3.0)
+    (:hue 0.0)
+    (T 0)))
 
 (defun gene-potential (genome)
   (declare (type keyword genome))
@@ -17,6 +17,17 @@
     (:body (loop for i from 0 below 8 collect i))
     (:face (loop for i from 0 below 8 collect i))
     (:hat (loop for i from 0 below 5 collect i))))
+
+(defun gene-random (genome)
+  (declare (type keyword genome))
+  (alexandria:random-elt (remove (gene-initial genome) (gene-potential genome))))
+
+(defun gene-random-speed (speed)
+  (declare (type keyword speed))
+  (let ((default (gene-initial :speed)))
+    (ecase speed
+      (:slow (alexandria:random-elt (mapcar #'(lambda (x) (< x default)) (gene-potential :speed))))
+      (:fast (alexandria:random-elt (mapcar #'(lambda (x) (< default x)) (gene-potential :speed)))))))
 
 (defun cross-gene (genome gene-a gene-b)
   (declare (type keyword genome))
--- genetics.lisp
+++ genetics.lisp
@@ -1,17 +1,15 @@
 (in-package #:org.shirakumo.fraf.vpetjam)
 
 (defclass genetical ()
-  ((genes :initform (make-hash-table) :reader genes)
-   (parents :initarg :parents :accessor parents))
-  (:default-initargs :parents NIL))
+  ((genes :initform (make-hash-table) :reader genes)))
 
 (defmethod shared-initialize :after ((critter genetical) slots &key genes)
+  (loop for genome in *genomes*
+        do (set-gene critter genome (gene-initial genome)))
   (if genes
       (loop for genome in genes by #'cddr
             for value in (rest genes) by #'cddr
-            do (set-gene critter genome value))
-      (loop for genome in (genomes)
-            do (set-gene critter genome (gene critter genome)))))
+            do (set-gene critter genome value))))
 
 (defmethod print-object ((critter genetical) stream)
   (print-unreadable-object (critter stream :type T :identity T)
@@ -20,17 +18,27 @@
 
 (defmethod gene ((critter genetical) genome)
   (declare (type keyword genome))
-  (gethash genome (genes critter) (gene-initial genome)))
+  (gethash genome (genes critter)))
 
 (defmethod set-gene ((critter genetical) genome value)
-  (declare (type number value))
-  (setf (gethash genome (genes critter)) value))
+  (declare (type keyword genome))
+  (declare (type (or keyword number) value))
+  (let ((value
+          (cond
+            ((eql value :random) (gene-random genome))
+            ((keywordp value)
+             (if (eql genome :speed)
+                 (gene-random-speed value)
+                 (error "Invalid gene value for ~a: ~a" genome value)))
+            (T value))))
+    (setf (gethash genome (genes critter)) value)))
 
 (defmethod gene-list ((critter genetical))
-  (alexandria:hash-table-plist (genes critter)))
+  (loop for genome in *genomes*
+        collect genome
+        collect (gene critter genome)))
 
 (defmethod cross ((critter genetical) (other genetical))
-  ;; TODO: Skipping generations.
-  (loop for genome in (genomes)
+  (loop for genome in *genomes*
         collect genome
         collect (cross-gene genome (gene critter genome) (gene critter genome))))
